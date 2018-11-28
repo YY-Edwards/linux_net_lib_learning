c0c70a059c2fdb7c33467b83113cddd1eecb497f
Edwards <330713927@qq.com>
2018-11-28 19:24:27 +0800

    继续研究logfile_test

--- muduo/base/tests/LogFile_test.cc
+++ muduo/base/tests/LogFile_test.cc
@@ -23,6 +23,7 @@ int main(int argc, char* argv[])
 {
   char name[256] = { 0 };
   strncpy(name, argv[0], sizeof name - 1);
+  //include <libgen.h> :basename
   g_logFile.reset(new muduo::LogFile(::basename(name), 200*1000));
   muduo::Logger::setOutput(outputFunc);
   muduo::Logger::setFlush(flushFunc);
